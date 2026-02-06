e08ec88a2925dc1a61f61a6ae18ffe40d5b1ea20
ygelfand <yuri@shlitz.com>
2026-02-06 07:24:10 -0500

    docs: update config doc
    Signed-off-by: ygelfand <yuri@shlitz.com>

---
+++ docs/content/_meta.js
@@ -0,0 +1,14 @@
+export default {
+  index: 'Overview',
+  'user-guide': {
+    type: 'separator',
+    title: 'User Guide'
+  },
+  tui: 'TUI Guide',
+  config: 'Configuration',
+  'reference': {
+    type: 'separator',
+    title: 'Reference'
+  },
+  cli: 'CLI Reference'
+}
---
+++ docs/content/cli/_meta.js
@@ -0,0 +1,9 @@
+export default {
+  'plexctl': 'plexctl Root',
+  '*': {
+    theme: {
+      breadcrumb: true,
+      pagination: true
+    }
+  }
+}
